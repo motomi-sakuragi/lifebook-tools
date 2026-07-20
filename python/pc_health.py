a7817022df19cecae80fddf3ac6366176edd9583
櫻木基充 <motomi0729@gmail.com>
2026-07-20 13:36:15 +0900

    Add detailed system information

--- python/pc_health.py
+++ python/pc_health.py
@@ -4,3 +4,7 @@ print("Project Compass")
 print("PC Health Checker")
 
 print(platform.system())
+print(platform.release())     # Windowsのリリース
+print(platform.version())     # 詳細バージョン
+print(platform.machine())     # CPUアーキテクチャ
+print(platform.processor())   # CPU情報
